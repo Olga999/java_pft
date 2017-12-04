eddf91175f5fdc026f293ef6db201eba5b67310a
Olga999 <34050867+Olga999@users.noreply.github.com>
2017-12-04 17:06:23 +0700

    Тест правильности вычисления расстояния

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTest.java
@@ -6,9 +6,10 @@ import org.testng.annotations.Test;
 public class PointTest {
 
   @Test
-  Point p1 = new Point(0, 0);
-  Point p2 = new Point(3, 4);
+  public void testPoint() {
+    Point p1 = new Point(0, 0);
+    Point p2 = new Point(3, 4);
 
-  Assert.assertEquals(p1.distance(p2)) == 5.0;
-
+    Assert.assertEquals(p1.distance(p2), 5.0);
+  }
 }
